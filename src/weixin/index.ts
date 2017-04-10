d96434ffbfb61d40826b643fd61367fc65f8845c
calidion <calidion@gmail.com>
2017-04-10 20:02:41 +0800

    updated

--- src/weixin/index.ts
+++ src/weixin/index.ts
@@ -1,8 +1,31 @@
 import * as express from 'express';
+import * as pay from 'node-weixin-pay';
+import config = require('../config/weixin');
+import * as qrcode from 'qrcode'
 
-export = function(app, models) {
+export = function (app, models) {
   let router = express.Router();
-  router.use('/pay/callback', function(req, res) {
+  router.use('/pay/qrcode', function (req, res) {
+    console.log('callback');
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
+    var opts = {
+      errorCorrectionLevel: 'H',
+      type: 'image/jpeg',
+      rendererOpts: {
+        quality: 0.3
+      }
+    };
+    var string = pay.qrcode(config.app, config.merchant, req.query.id || 'id');
+    console.log(string);
+    qrcode.toDataURL(string, opts, function (err, url) {
+      if (err) throw err;
+      res.end("<!DOCTYPE html/><html><head><title>node-qrcode</title></head><body><img src='" + url + "'/></body></html>")
+    });
+  });
+  
+  router.use('/pay/callback', function (req, res) {
     console.log('callback');
     console.log(req.params);
     console.log(req.query);
