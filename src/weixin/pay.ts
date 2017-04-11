c2a5c49e05b1327d8c45517a525dd691f78fe074
calidion <calidion@gmail.com>
2017-04-11 09:00:55 +0800

    .

---
+++ src/weixin/pay.ts
@@ -0,0 +1,46 @@
+import * as pay from 'node-weixin-pay';
+
+export class WeiXinPay {
+  config
+  constructor(config) {
+    this.config = config;
+  }
+
+  qrString(...arg) {
+    pay.qrcode.apply(pay, arg);
+  }
+
+  preparePublicPay(data) {
+    data.trade_type = 'JSSDK';
+  }
+
+  prepareQRScanPay(data) {
+    data.trade_type = 'NATIVE';
+  }
+
+  preparePayType(data, type = 'JSSDK') {
+    data.trade_type = type;
+  }
+  prepareClientInfo(data, req) {
+    var ip = req.headers['x-forwarded-for'] ||
+      (req.connection ? req.connection.remoteAddress : null) || req.ip;
+    ip = ip.split(',')[0];
+    data.spbill_create_ip = ip;
+  }
+  prepareOrderInfo(data, order) {
+    data.body = order.title;
+    data.out_trade_no = String(order.no);
+    data.total_fee = ((order.price - 0) * 100).toFixed(0);
+  }
+
+  prepareUserInfo(data, user) {
+    data.openid = user.openid.openid;
+  }
+
+  qrPay(data, type, order, user, req) {
+    this.preparePayType(data, type);
+    this.prepareClientInfo(data, req);
+    this.prepareOrderInfo(data, order);
+    this.prepareUserInfo(data, user);
+  }
+}
